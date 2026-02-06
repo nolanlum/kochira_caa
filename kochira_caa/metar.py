f85aeec6c8e7086e5d7a0145e1329b54bae40e39
Nolan Lum <nolan@nolm.name>
2026-02-05 18:16:08 -0800

    [metar] use api key boo

--- kochira_caa/metar.py
+++ kochira_caa/metar.py
@@ -6,9 +6,16 @@ For SZI and SZI accessories.
 
 import requests
 
-from kochira.service import Service
+from kochira import config
+from kochira.service import Service, Config
 
 service = Service(__name__, __doc__)
+
+
+@service.config
+class Config(Config):
+    api_key = config.Field(doc="AVWX app token.")
+
 
 @service.command(r"!metar (?P<location>[A-Z0-9]{4})$")
 def metar(ctx, location):
@@ -18,12 +25,17 @@ def metar(ctx, location):
     Get the METAR for a specific station by ICAO ident.
     """
     response = requests.get(
-        "https://avwx.rest/api/preview/metar/{location}".format(location=location),
+        "https://avwx.rest/api/metar/{location}".format(location=location),
         params={
+            'token': ctx.config.api_key,
             'format': 'json',
             'onfail': 'cache',
         },
     ).json()
+
+    if 'error' in response:
+        ctx.respond(response['error'])
+        return
 
     ctx.respond(response['sanitized'])
 
